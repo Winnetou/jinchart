b8b2750e491e7523e003f1c0295720377c0de522
Winnetou <mieszko.chowaniec@gmail.com>
2014-12-07 15:32:12 +0100

    tests

---
+++ tests.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from random import choice
+
+from jinchart import BarChart, LineChart, PieChart
+
+class JinChartTest(unittest.TestCase):
+
+    def test_barchart_with_dict():
+        ''' pass a dict to barchart'''
+        #TODO
+        raise AssertionError
+
+    def test_linechart_with_dict():
+        ''' pass a dict to linechart'''
+        #TODO
+        raise AssertionError
+
+    def test_piechart_with_dict():
+        ''' pass a dict to piechart'''
+        #TODO
+        raise AssertionError
+
+    def test_barchart_with_list_or_tuple():
+        ''' pass a list or a tuple to barchart'''
+        #TODO
+        raise AssertionError
+
+    def test_linechart_with_list_or_tuple():
+        ''' pass a list or a tuple to linechart'''
+        #TODO
+        raise AssertionError
+
+    def test_piechart_with_list_or_tuple():
+        ''' pass a list or a tuple to piechart'''
+        #TODO
+        raise AssertionError
