5145db2da93a481b62563ab67fb8b854f516e2ec
Winnetou <mieszko.chowaniec@gmail.com>
2014-12-07 13:23:42 +0100

    initial

---
+++ jinchart.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#chart js filter
+
+
+def BarChart():
+    return mark_safe(result)
+
+def LineChart():
+    return mark_safe(result)
+
+def PieChart():
+    return mark_safe(result)
